6bc4968bf3acf6ed647173b1df81d3b3a9c36f5a
dominik-polic <dominik.polic@gmail.com>
2022-08-30 16:25:34 +0200

    fix gpio and hw_timer

--- components/esp8266/driver/hw_timer.c
+++ components/esp8266/driver/hw_timer.c
@@ -159,15 +159,25 @@ uint32_t hw_timer_get_count_data()
     return frc1.count.data;
 }
 
-static void IRAM_ATTR hw_timer_isr_cb(void* arg)
+static void IRAM_ATTR hw_timer_isr_cb(void *arg)
 {
-    if (!frc1.ctrl.reload) {
-        frc1.ctrl.en = 0;
+    typeof ( ((frc1_struct_t*)0)->ctrl) ctrl;
+    ctrl.val = frc1.ctrl.val;
+    if (!ctrl.reload) {
+        ctrl.en = 0;
+        frc1.ctrl.val = ctrl.val; //stop counter
+        /* frc1.count.val = 0 causes continuous fire even with the timer disabled. 
+         * So, an immediate reload is forced to avoid that (assuming that the current load value is valid).
+         */ 
+        uint32_t val = frc1.load.val; //split to suppress a warning
+        frc1.load.val= val;
+        soc_clear_int_mask(1ul << (ETS_FRC_TIMER1_INUM));
     }
     if (hw_timer_obj->cb != NULL) {
         hw_timer_obj->cb(arg);
     }
 }
+
 
 esp_err_t hw_timer_disarm(void)
 {
